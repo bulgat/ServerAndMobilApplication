4ae06d35d9e90f3adf4a235660160142951d1a54
bulgat <sirbulgat@gmail.com>
2025-11-29 11:54:20 +0300

    FULL CHANGE NET CORE REACT

---
+++ ServerReglamentAPI/ServerAndMobil.Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using TriangleDocker.dataBasa;
+
+namespace ServerAndMobil.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AppDBcontent _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, AppDBcontent context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var scoreList = _context.Score.ToList();
+
+            var kol = 99;
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ServerReglamentAPI/ServerAndMobil.Server/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using TriangleDocker.dataBasa;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDBcontent>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
