cb095c1b43de52c74532d274d948f7d9397021d9
Nikolay K <sirbulgat@gmail.com>
2025-12-03 23:26:08 +0300

    add graphQL first.

--- ServerReglamentAPI/serverandmobil.client/src/main.jsx
+++ ServerReglamentAPI/serverandmobil.client/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+//import { HttpLink } from 'apollo-link-http';
 
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+/*
+const httpLink = new HttpLink({
+    uri: GITHUB_BASE_URL,
+    headers: {
+        //authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+            //}`,
+    },
+});
+
+const client = new ApolloClient({
+    //link: httpLink,
+    uri: "YOUR_GRAPHQL_API_ENDPOINT",
+    cache: new InMemoryCache(),
+});
+*/
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+    <StrictMode>
+ 
+        <App />
+ 
   </StrictMode>,
 )
